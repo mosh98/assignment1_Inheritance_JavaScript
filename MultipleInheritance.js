67590f6e0fcc8c52ca4a765c46b82048d962bad3
moslehmahamud <mosleh.edu@gmail.com>
2019-11-27 13:51:54 +0100

    scrrr scrr

--- MultipleInheritance.js
+++ MultipleInheritance.js
@@ -1,21 +1,35 @@
 
 var myObject = {
-    create: function (a) {
-        if(a === null)
-            console.log("man wtf")
 
-        if (Array.isArray(a)) {
+
+    create: function (a) {
+
+        if (a === null)
+            console.log("man wtf")
+        else if (Array.isArray(a)) {
             console.log("terry crews")
             /*** this where inheritance happnes*/
-            a.forEach(item, index, a)
-           item.prototype
+            /*  var arrayLength = a.length;
+              for (var i = 0; i < arrayLength; i++) {
+                  //Do something
+                  b.push(a[i]);
+                  Object.assign(this.prototype,a[i]);
+              }*/
+
+
         }
     },
 
-    call: function (obj,parametres) {
-        for(var m in a) {
-            if(typeof a[m] == obj) {
-                return a[m].obj(parametres);
+    call: function (obj, parametres) {
+        var funcName = obj;
+        var parametreName = parametres;
+        var arrayLength = a.length;
+        for (var i = 0; i < arrayLength; i++) {
+            console.log();
+            //Do something
+            if (a[i].hasOwnProperty(obj)) {
+                var temp = a[i];
+                return temp(parametres);
             }
         }
     }
@@ -27,5 +41,5 @@ obj0.func = function(arg) { return "func0: " + arg; };
 obj1 = myObject.create([obj0]);
 obj2 = myObject.create([]);
 obj3 = myObject.create([obj2, obj1]);
-//result = obj3.call("func", ["hello"]);
-//console.log("should print ’func0: hello’ ->", result);
+result = obj3.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);
---
+++ MultipleInheritance2.js
@@ -0,0 +1,17 @@
+var myObject = {}
+
+myObject.call(obj,parametre)
+
+myObject.create = function(a){
+
+    if (a === null)
+        console.log("man wtf")
+    else if (Array.isArray(a)) {
+        console.log("terry crews")
+        /*** this where inheritance happnes*/
+
+
+    }
+}
+
+myObject.call(obj,parametre)
