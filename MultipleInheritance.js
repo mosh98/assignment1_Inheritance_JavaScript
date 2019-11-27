6405a1e39a64cdfab7acaed0b7bfef69af4c0ebc
moslehmahamud <mosleh.edu@gmail.com>
2019-11-27 16:09:28 +0100

    en liten bit i alla fall

--- MultipleInheritance.js
+++ MultipleInheritance.js
@@ -1,47 +1,49 @@
 
-var myObject = {}
+var myObject = {
+
+}
+
+
+
+myObject.create = function (a) {
+
+    var b = a
+
+    if (a === null) {
+        console.log("man wtf")
+        return Object
+    } else if (Array.isArray(a)) {
+        //console.log("terry crews")
+        /*** this where inheritance happnes*/
+        var arrayLength = a.length;
+        for (var i = 0; i < arrayLength; i++) {
+            //Do something
+            b.push(a[i]);
+        }
+        return myObject
+    }
+}
 
 myObject.call = function (obj, parametres) {
     var funcName = obj;
     var parametreName = parametres;
-    var arrayLength = b.length;
 
-    if(this.prototype.hasOwnProperty(funcName)){
-        var temp = this.prototype.funcName;
-        return temp(funcName);
-    }
+    var arr = Array.from('create')
+    var arrayLength = arr.length;
+    /*    if(this.prototype.hasOwnProperty(funcName)){
+            var temp = this.funcName;
+            return temp(funcName);
+        }*/
 
     for (var i = 0; i < arrayLength; i++) {
-        console.log();
+        console.log("call loop initated");
         //Do something
-        if (b[i].name === funcName) {
-            var temp = b[i];
-            return temp(parametres);
+        if (arr[i].name === funcName) {
+            var temp = arr[i];
+            return temp(parametreName);
         }
     }
-},
-    myObject.create = function (a) {
-
-    var b = a
-
-        if (a === null){
-            console.log("man wtf")
-            return Object
-        }
-
-
-    else if (Array.isArray(a)) {
-            console.log("terry crews")
-            /*** this where inheritance happnes*/
-              var arrayLength = a.length;
-              for (var i = 0; i < arrayLength; i++) {
-                //Do something
-                b.push(a[i]);
-            }
-              return Object
-        }
-    }
-
+}
 
 
 var obj0 = myObject.create(null);
@@ -50,5 +52,5 @@ var obj1 = myObject.create([obj0]);
 var obj2 = myObject.create([]);
 obj2.func = function(arg) { return "func2: " + arg; };
 var obj3 = myObject.create([obj1, obj2]);
-var result = obj3.call("func", ["hello"]) ;
+var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
