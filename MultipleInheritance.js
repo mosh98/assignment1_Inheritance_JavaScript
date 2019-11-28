d15f0839cc4fc7557264fc632ec4e882da8f2c4c
moslehmahamud <mosleh.edu@gmail.com>
2019-11-28 09:22:54 +0100

    inte klar

--- MultipleInheritance.js
+++ MultipleInheritance.js
@@ -1,13 +1,16 @@
-
-var myObject = {
-
-}
+var myObject = {}
 
 
 myObject.create = function (protoList) {
+
     var temp = {}
-   temp.__proto__ = this
-    temp.list = protoList
+    temp.__proto__ = this
+
+    if(protoList === null){
+        temp.list = []
+    }else {
+        temp.list = protoList
+    }
     return temp
 }
 
@@ -16,33 +19,41 @@ myObject.call = function (nameOfFunction, parametres) {
     var arr = this.list
     var arrayLength = arr.length;
 
-        var smth;
+
 
     for (var i = 0; i < arrayLength; i++) {
-
         var okoko = arr[i];
         if (okoko.hasOwnProperty(funcName)) {
             console.log("function Exist ? :" + okoko.hasOwnProperty(funcName))
 
-            var temp = arr[i];
-            smth = arr[i];
-
-           // return smth.funcName.apply(parametres)
-            break;
+            return arr[i][funcName](parametres);
         }
     }
-
-    return smth[funcName](parametres);
 
 
 }
 
 
 var obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
+obj0.func = function (arg) {return "func0: " + arg;};
 var obj1 = myObject.create([obj0]);
 var obj2 = myObject.create([]);
-obj2.func = function(arg) { return "func2: " + arg; };
+obj2.func = function (arg) {return "func2: " + arg;};
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
+/*
+obj0 = myObject.create(null);
+obj0.func = function (arg) {
+    return "func0: " + arg;};
+obj1 = myObject.create([obj0]);
+obj2 = myObject.create([]);
+obj3 = myObject.create([obj2, obj1]);
+result = obj3.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);*/
+
+/*
+obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+result = obj0.call("func", ["hello"]);
+console.log("should print ’func0: hello’ ->", result);*/
